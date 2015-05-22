c4647d3690527861e668fbecf8b34342ced13b87
Augusto Leao <aleao@summasolutions.net>
2015-05-21 21:15:09 -0300

    prusa guga

--- Marlin/temperature.cpp
+++ Marlin/temperature.cpp
@@ -1083,6 +1083,7 @@ void disable_heater()
 }
 
 void max_temp_error(uint8_t e) {
+  return;
   disable_heater();
   if(IsStopped() == false) {
     SERIAL_ERROR_START;
@@ -1109,6 +1110,7 @@ void min_temp_error(uint8_t e) {
 }
 
 void bed_max_temp_error(void) {
+return;
 #if HEATER_BED_PIN > -1
   WRITE(HEATER_BED_PIN, 0);
 #endif
